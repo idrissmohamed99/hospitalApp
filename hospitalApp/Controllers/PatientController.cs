62f43689b187ee03bfff81d51a577a01059ea4a0
Ahmed-Mohamed-Hussin <36509814+idrissmohamed99@users.noreply.github.com>
2020-08-27 22:05:49 +0200

    Add project files.

---
+++ hospitalApp/Controllers/PatientController.cs
@@ -0,0 +1,51 @@
+﻿using hospitalApp.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hospitalApp.Controllers
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient
+        hospiitalDbEntities db = new hospiitalDbEntities();
+
+        [HttpGet]
+        public ActionResult GetPatient()
+        {
+            return View(db.Patients.ToList());
+
+        }
+
+
+
+        // Get  action for insert secreen
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Patients.Add(patient);
+                db.SaveChanges();
+                return RedirectToAction("GetPatient");
+
+            }
+            return View();
+
+        }
+
+
+
+
+
+    }
+}
